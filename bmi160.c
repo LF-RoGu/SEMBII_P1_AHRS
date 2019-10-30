5b32f851454dacb153cf2c74d9f120029feff677
Luis <36269176+LProtox@users.noreply.github.com>
2019-10-30 17:32:53 -0600

    AHRS RTOS lecture implemented
    Still missing the data send to the PC via mahony functions

--- bmi160.c
+++ bmi160.c
@@ -1,13 +1,12 @@
 /*
  * bmi160.c
  *
- *  Created on: Oct 25, 2019
+ *  Created on: Oct 29, 2019
  *      Author: LuisFernando
  */
 
 #include "bmi160.h"
 
-uint8_t g_MasterCompletionFlag = false;
 
 /* Buffer to read acc & gyr*/
 uint8_t g_read_gyr[6] = {0};
@@ -16,152 +15,148 @@ uint8_t g_read_acc[6] = {0};
 uint16_t g_data_axis_acc[3] = {0};
 uint16_t g_data_axis_gyr[3] = {0};
 
-static i2c_master_handle_t g_master_handle;
-static i2c_master_transfer_t g_master_transfer;
-static i2c_master_config_t g_master_config;
+uint8_t header_counter = 0;
+
+float acc_var_x [BMI160_VAR_BUFFER_SIZE] = {0};
+uint8_t acc_index_x = 0;
+float acc_var_y [BMI160_VAR_BUFFER_SIZE] = {0};
+uint8_t acc_index_y = 0;
+float acc_var_z [BMI160_VAR_BUFFER_SIZE] = {0};
+uint8_t acc_index_z = 0;
+
+float gyr_var_x [BMI160_VAR_BUFFER_SIZE] = {0};
+uint8_t gyr_index_x = 0;
+float gyr_var_y [BMI160_VAR_BUFFER_SIZE] = {0};
+uint8_t gyr_index_y = 0;
+float gyr_var_z [BMI160_VAR_BUFFER_SIZE] = {0};
+uint8_t gyr_index_z = 0;
+
+float acc_var[3] = {0};
+float gyr_var[3] = {0};
 
 struct bmi160_device sensor;
 struct comm_msg_acc_t acc_device;
 struct comm_msg_acc_t gyr_device;
 
-static void i2c_master_callback(
-		I2C_Type *base,
-		i2c_master_handle_t *handle,
-        status_t status,
-		void * userData)
-{
-	//PRINTF("SUCCESS\n");
-	if (status == kStatus_Success)
-	{
-		g_MasterCompletionFlag = true;
-	}
-}
+struct rtos_i2c_bmi160_package sensor_package;
 
-/*!
- * @brief This API reads the data from the given register address
- * of sensor.
- */
-void gpio_i2c_config(void)
-{
-	I2C_MasterGetDefaultConfig(&g_master_config);
-
-	g_master_config.baudRate_Bps = BMI160_BAUDRATE;
-
-	I2C_MasterInit(I2C0, &g_master_config, CLOCK_GetFreq(kCLOCK_BusClk));
-
-	I2C_MasterTransferCreateHandle(I2C0, &g_master_handle,i2c_master_callback, NULL);
-}
 /*!
  * @brief This API
  */
 void bmi160_normal_mode_config(void)
 {
-	/*
-	 * ACC
-	 */
-	/* Select the Output data rate, range of accelerometer sensor */
-	sensor.accel_cfg.odr = BMI160_ACCEL_ODR_1600HZ;
-	sensor.accel_cfg.range = BMI160_ACCEL_RANGE_2G;
-	sensor.accel_cfg.bw = BMI160_ACCEL_BW_NORMAL_AVG4; //en teoria es un 0x20
-	/* Select the power mode of accelerometer sensor */
-	sensor.accel_cfg.power = BMI160_ACCEL_NORMAL_MODE;
-	/*
-	 * GYRO
-	 */
-	/* Select the Output data rate, range of Gyroscope sensor */
-	sensor.gyro_cfg.odr = BMI160_GYRO_ODR_3200HZ;
-	sensor.gyro_cfg.range = BMI160_GYRO_RANGE_2000_DPS;
-	sensor.gyro_cfg.bw = BMI160_GYRO_BW_NORMAL_MODE;
-	/* Select the power mode of Gyroscope sensor */
-	sensor.gyro_cfg.power = BMI160_GYRO_NORMAL_MODE;
 
-	/* Send the Data to the sensor*/
-
-	/* SET POWER MODE*/
-	/* ACC*/
-	bmi160_write(BMI160_COMMAND_REG_ADDR,sensor.accel_cfg.power);
-	/* GYR*/
-	bmi160_write(BMI160_COMMAND_REG_ADDR,sensor.gyro_cfg.power);
-
-	/* ACC*/
-	bmi160_write(BMI160_ACC_CONF,(sensor.accel_cfg.bw | sensor.accel_cfg.odr));
-	bmi160_write(BMI160_ACC_RANGE,sensor.accel_cfg.range);
-
-	/* GYR*/
-	bmi160_write(BMI160_GYR_CONF,(sensor.gyro_cfg.bw | sensor.gyro_cfg.odr));
-	bmi160_write(BMI160_GYR_RANGE,sensor.gyro_cfg.range);
-}
-/************************************************************************************/
-/*!
- * @brief This API
- */
-void bmi160_write(uint16_t reg,uint8_t data)
-{
-	g_master_transfer.slaveAddress = BMI160_I2C_ADDR;
-	g_master_transfer.direction = kI2C_Write;
-	g_master_transfer.subaddress = reg;
-	g_master_transfer.subaddressSize = BMI160_SUBADDRESS_SIZE;
-	g_master_transfer.data = &data;
-	g_master_transfer.dataSize = BMI160_DATA_SIZE;
-	g_master_transfer.flags = kI2C_TransferDefaultFlag;
-
-	I2C_MasterTransferNonBlocking(I2C0, &g_master_handle,&g_master_transfer);
-
-	while (!g_MasterCompletionFlag)
+	while(TRUE)
 	{
+		/* SOFT RESET*/
+		sensor_package.i2c_number = rtos_i2c_0;
+		sensor_package.buffer = BMI160_SOFT_RESET_CMD;
+		sensor_package.length = BMI160_DATA_SIZE;
+		sensor_package.slave_addr = BMI160_I2C_ADDR;
+		sensor_package.subaddr = BMI160_COMMAND_REG_ADDR;
+		sensor_package.subsize = BMI160_SUBADDRESS_SIZE;
+
+		rtos_i2c_transfer(
+				sensor_package.i2c_number,
+				sensor_package.buffer,
+				sensor_package.length,
+				sensor_package.slave_addr,
+				sensor_package.subaddr,
+				sensor_package.subsize);
 		/*
-		 * Do nothing
+		 * ACC
 		 */
+		/* Select the Output data rate, range of accelerometer sensor */
+		sensor.accel_cfg.odr = BMI160_ACCEL_ODR_1600HZ;
+		sensor.accel_cfg.range = BMI160_ACCEL_RANGE_2G;
+		/* Select the power mode of accelerometer sensor */
+		sensor.accel_cfg.power = BMI160_ACCEL_NORMAL_MODE;
+		/*
+		 * GYRO
+		 */
+		/* Select the Output data rate, range of Gyroscope sensor */
+		sensor.gyro_cfg.odr = BMI160_GYRO_ODR_3200HZ;
+		sensor.gyro_cfg.range = BMI160_GYRO_RANGE_2000_DPS;
+		/* Select the power mode of Gyroscope sensor */
+		sensor.gyro_cfg.power = BMI160_GYRO_NORMAL_MODE;
+
+
+		/* Sensor package*/
+		/* ACC*/
+		sensor_package.i2c_number = rtos_i2c_0;
+		sensor_package.buffer = &sensor.accel_cfg.power;
+		sensor_package.length = BMI160_DATA_SIZE;
+		sensor_package.slave_addr = BMI160_I2C_ADDR;
+		sensor_package.subaddr = BMI160_COMMAND_REG_ADDR;
+		sensor_package.subsize = BMI160_SUBADDRESS_SIZE;
+
+		/* SET POWER MODE*/
+		rtos_i2c_transfer(
+				sensor_package.i2c_number,
+				sensor_package.buffer,
+				sensor_package.length,
+				sensor_package.slave_addr,
+				sensor_package.subaddr,
+				sensor_package.subsize);
+
+		/* GYR*/
+		sensor_package.i2c_number = rtos_i2c_0;
+		sensor_package.buffer = &sensor.gyro_cfg.power;
+		sensor_package.length = BMI160_DATA_SIZE;
+		sensor_package.slave_addr = BMI160_I2C_ADDR;
+		sensor_package.subaddr = BMI160_COMMAND_REG_ADDR;
+		sensor_package.subsize = BMI160_SUBADDRESS_SIZE;
+
+		/* SET POWER MODE*/
+		rtos_i2c_transfer(
+				sensor_package.i2c_number,
+				sensor_package.buffer,
+				sensor_package.length,
+				sensor_package.slave_addr,
+				sensor_package.subaddr,
+				sensor_package.subsize);
+
+		/*Kill task*/
+		vTaskSuspend(NULL);
+
 	}
-	g_MasterCompletionFlag = false;
-
-	I2Cwritedelay();
 }
-/*!
- * @brief This API
- */
-uint8_t bmi160_read(uint16_t reg)
-{
-	uint8_t data_temp;
 
-	g_master_transfer.slaveAddress = BMI160_I2C_ADDR;
-	g_master_transfer.direction = kI2C_Read;
-	g_master_transfer.subaddress = reg;
-	g_master_transfer.subaddressSize = BMI160_SUBADDRESS_SIZE;
-	g_master_transfer.data = &data_temp;
-	g_master_transfer.dataSize = BMI160_DATA_SIZE;
-	g_master_transfer.flags = kI2C_TransferDefaultFlag;
-
-	I2C_MasterTransferNonBlocking(I2C0, &g_master_handle,&g_master_transfer);
-
-	while (!g_MasterCompletionFlag)
-	{
-	}
-	g_MasterCompletionFlag = false;
-
-	I2Cwritedelay();
-
-	return data_temp;
-}
 /*!
  * @brief This API
  */
 void bmi160_read_acc(void)
 {
 	uint8_t index = 0;
+
 	uint8_t data_temp;
 
-	uint16_t reg_address = BMI160_READ_ACC_X_L;
+	/* Sensor package*/
+	/* ACC*/
+	sensor_package.i2c_number = rtos_i2c_0;
+	sensor_package.buffer = &data_temp;
+	sensor_package.length = BMI160_DATA_SIZE;
+	sensor_package.slave_addr = BMI160_I2C_ADDR;
+	sensor_package.subaddr = BMI160_READ_ACC_X_L;
+	sensor_package.subsize = BMI160_SUBADDRESS_SIZE;
 
-	for(index = X_LOW; index <= Z_HIGH; index++)
+	for(index = X_LOW; index < Z_HIGH; index++)
 	{
-		data_temp = bmi160_read(reg_address);
-		g_read_acc[index] = data_temp;
+		rtos_i2c_receive(
+						sensor_package.i2c_number,
+						sensor_package.buffer,
+						sensor_package.length,
+						sensor_package.slave_addr,
+						sensor_package.subaddr,
+						sensor_package.subsize);
 
-		reg_address++;
+		g_read_acc[index] = *sensor_package.buffer;
+
+		sensor_package.subaddr++;
 	}
-
-	/**/
+	/*
+	 *
+	 */
 	data_axis_acc();
 }
 /*!
@@ -170,21 +165,58 @@ void bmi160_read_acc(void)
 void bmi160_read_gyr(void)
 {
 	uint8_t index = 0;
+
 	uint8_t data_temp;
 
-	uint16_t reg_address = BMI160_READ_GYR_X_L;
+	/* Sensor package*/
+	/* ACC*/
+	sensor_package.i2c_number = rtos_i2c_0;
+	sensor_package.buffer = &data_temp;
+	sensor_package.length = BMI160_DATA_SIZE;
+	sensor_package.slave_addr = BMI160_I2C_ADDR;
+	sensor_package.subaddr = BMI160_READ_GYR_X_L;
+	sensor_package.subsize = BMI160_SUBADDRESS_SIZE;
 
-	for(index = X_LOW; index <= Z_HIGH; index++)
+	for(index = X_LOW; index < Z_HIGH; index++)
 	{
-		data_temp = bmi160_read(reg_address);
-		g_read_gyr[index] = data_temp;
+		rtos_i2c_receive(
+						sensor_package.i2c_number,
+						sensor_package.buffer,
+						sensor_package.length,
+						sensor_package.slave_addr,
+						sensor_package.subaddr,
+						sensor_package.subsize);
 
-		reg_address++;
+		g_read_gyr[index] = *sensor_package.buffer;
+
+		sensor_package.subaddr++;
 	}
 
-	/**/
+	/*
+	 *
+	 */
 	data_axis_gyr();
 }
+
+/*!
+ * @brief This API
+ */
+void bmi160_read_acc_gyr(void)
+{
+	while(TRUE)
+	{
+		/* Read values and stores it in the acc and gyt structure*/
+		bmi160_read_acc();
+		bmi160_read_gyr();
+
+		/* Get the var of both data*/
+		//bmi160_varianza();
+
+		/* Send data to the mahony*/
+		bmi160_send_mahony();
+	}
+}
+
 /*!
  * @brief This API
  */
@@ -205,12 +237,12 @@ void data_axis_acc(void)
 
 	g_data_axis_acc[Z_REG] = data_temp;
 
-
-	/* Print data axis*/
-	bmi160_print_acc_dec();
-	/* Convert data axis*/
+	/*
+	 *
+	 */
 	convert_value_acc(g_data_axis_acc);
 }
+
 /*!
  * @brief This API
  */
@@ -231,52 +263,10 @@ void data_axis_gyr(void)
 
 	g_data_axis_gyr[Z_REG] = data_temp;
 
-
-	/* Print data axis in uint32_t*/
-	bmi160_print_gyr_dec();
-	/* Convert data axis*/
+	/*
+	 *
+	 */
 	convert_value_gyr(g_data_axis_gyr);
-}
-/*!
- * @brief This API
- */
-void bmi160_print_acc_dec(void)
-{
-	printf("acc axis: X: %d Y: %d Z: %d\n", g_data_axis_acc[X_REG],g_data_axis_acc[Y_REG],g_data_axis_acc[Z_REG]);
-}
-
-/*!
- * @brief This API
- */
-void bmi160_print_acc_float(void)
-{
-	printf("FLOAT acc axis: X: %.2f Y: %.2f Z: %.2f\n", acc_device.x,acc_device.y,acc_device.z);
-}
-
-/*!
- * @brief This API
- */
-void bmi160_print_gyr_dec(void)
-{
-	printf("gyr axis: X: %d Y: %d Z: %d\n", g_data_axis_gyr[X_REG],g_data_axis_gyr[Y_REG],g_data_axis_gyr[Z_REG]);
-}
-
-/*!
- * @brief This API
- */
-void bmi160_print_gyr_float(void)
-{
-	printf("FLOAT gyr axis: X: %.2f Y: %.2f Z: %.2f\n", gyr_device.x,gyr_device.y,gyr_device.z);
-}
-
-/*!
- * @brief This API
- */
-void bmi160_get_data(void)
-{
-	/**/
-	bmi160_read_acc();
-	bmi160_read_gyr();
 }
 
 /*!
@@ -345,8 +335,6 @@ void convert_value_acc(uint16_t *acc_axis_data)
 			break;
 		}
 	}
-	/* Print data axis in float*/
-	bmi160_print_acc_float();
 }
 
 /*!
@@ -415,16 +403,153 @@ void convert_value_gyr(uint16_t *gyr_axis_data)
 			break;
 		}
 	}
-	/* Print data axis in float*/
-	bmi160_print_gyr_float();
 }
+
 /*!
  * @brief This API
  */
-void I2Cwritedelay(void)
+void bmi160_varianza(void)
 {
-	for(uint32_t i = 120000000;i==0;i--)
-	{
-	}
+	float media_gyr_x = 0;
+	float media_gyr_y = 0;
+	float media_gyr_z = 0;
 
+	float media_acc_x = 0;
+	float media_acc_y = 0;
+	float media_acc_z = 0;
+	/*
+	 *
+	 */
+	acc_device.header = header_counter;
+
+	acc_var_x[acc_index_x] = acc_device.x;
+	acc_index_x++;
+	acc_var_y[acc_index_y] = acc_device.y;
+	acc_index_y++;
+	acc_var_z[acc_index_z] = acc_device.z;
+	acc_index_z++;
+
+	/*
+	 *
+	 */
+	gyr_device.header = header_counter;
+
+	gyr_var_x[gyr_index_x] = gyr_device.x;
+	gyr_index_x++;
+	gyr_var_y[gyr_index_y] = gyr_device.y;
+	gyr_index_y++;
+	gyr_var_z[gyr_index_z] = gyr_device.z;
+	gyr_index_z++;
+
+	header_counter++;
+
+	if(BMI160_VAR_BUFFER_SIZE == header_counter)
+	{
+		/* do var calculus*/
+		uint8_t index = 0;
+		for(index = 0; index <= BMI160_VAR_BUFFER_SIZE; index++)
+		{
+			acc_var[X_REG] += acc_var_x[index];
+			gyr_var[X_REG] += gyr_var_x[index];
+
+			acc_var[Y_REG] += acc_var_y[index];
+			gyr_var[Y_REG] += gyr_var_y[index];
+
+			acc_var[Z_REG] += acc_var_z[index];
+			gyr_var[Z_REG] += gyr_var_z[index];
+			if(BMI160_VAR_BUFFER_SIZE == index)
+			{
+				/* Calcula la media de la muestra*/
+				media_acc_x = acc_var[X_REG] / BMI160_VAR_BUFFER_SIZE;
+				media_gyr_x = gyr_var[X_REG] / BMI160_VAR_BUFFER_SIZE;
+
+				media_acc_x = acc_var[Y_REG] / BMI160_VAR_BUFFER_SIZE;
+				media_gyr_x = gyr_var[Y_REG] / BMI160_VAR_BUFFER_SIZE;
+
+				media_acc_x = acc_var[Z_REG] / BMI160_VAR_BUFFER_SIZE;
+				media_gyr_x = gyr_var[Z_REG] / BMI160_VAR_BUFFER_SIZE;
+
+				/* Calcula la varianza de la muestra*/
+
+				uint8_t index_var = 0;
+				for(index_var = 0; index_var < BMI160_VAR_BUFFER_SIZE; index_var++)
+				{
+					/* Varianza de las muestras*/
+					acc_var[X_REG] += ((acc_var_x[index_var] - media_acc_x) * (acc_var_x[index_var] - media_acc_x)) / (BMI160_VAR_BUFFER_SIZE - 1);
+					gyr_var[X_REG] += ((gyr_var_x[index_var] - media_gyr_x) * (gyr_var_x[index_var] - media_gyr_x)) / (BMI160_VAR_BUFFER_SIZE - 1);
+
+					acc_var[Y_REG] += ((acc_var_y[index_var] - media_acc_y) * (acc_var_y[index_var] - media_acc_y)) / (BMI160_VAR_BUFFER_SIZE - 1);
+					gyr_var[Y_REG] += ((gyr_var_y[index_var] - media_gyr_y) * (gyr_var_y[index_var] - media_gyr_y)) / (BMI160_VAR_BUFFER_SIZE - 1);
+
+					acc_var[Z_REG] += ((acc_var_z[index_var] - media_acc_z) * (acc_var_z[index_var] - media_acc_z)) / (BMI160_VAR_BUFFER_SIZE - 1);
+					gyr_var[Z_REG] += ((gyr_var_z[index_var] - media_gyr_z) * (gyr_var_z[index_var] - media_gyr_z)) / (BMI160_VAR_BUFFER_SIZE - 1);
+				}
+				/* Desviacion estardar*/
+				acc_var[X_REG] = sqrt(acc_var[X_REG]);
+				gyr_var[X_REG] = sqrt(gyr_var[X_REG]);
+
+				acc_var[Y_REG] = sqrt(acc_var[Y_REG]);
+				gyr_var[Y_REG] = sqrt(gyr_var[Y_REG]);
+
+				acc_var[Z_REG] = sqrt(acc_var[Z_REG]);
+				gyr_var[Z_REG] = sqrt(gyr_var[Z_REG]);
+			}
+		}
+		/* rst buffer counter*/
+		if((BMI160_VAR_BUFFER_SIZE == gyr_device.header) && (BMI160_VAR_BUFFER_SIZE == acc_device.header))
+		{
+			acc_index_x = 0;
+			acc_index_y = 0;
+			acc_index_z = 0;
+
+			gyr_index_x = 0;
+			gyr_index_y = 0;
+			gyr_index_z = 0;
+
+			header_counter = 0;
+		}
+	}
+}
+
+/*!
+ * @brief This API
+ */
+void bmi160_send_mahony(void)
+{
+	while(TRUE)
+	{
+		MahonyAHRSupdateIMU(gyr_device.x,gyr_device.y,gyr_device.z,acc_device.x,acc_device.y,acc_device.z);
+	}
+}
+
+/*!
+ * @brief This API
+ */
+void bmi160_print_acc_dec(void)
+{
+	printf("acc axis: X: %d Y: %d Z: %d\n", g_data_axis_acc[X_REG],g_data_axis_acc[Y_REG],g_data_axis_acc[Z_REG]);
+}
+
+/*!
+ * @brief This API
+ */
+void bmi160_print_acc_float(void)
+{
+	printf("acc axis: X: %f Y: %f Z: %f\n", acc_device.x,acc_device.y,acc_device.z);
+}
+
+/*!
+ * @brief This API
+ */
+void bmi160_print_gyr_dec(void)
+{
+	printf("gyr axis: X: %d Y: %d Z: %d\n", g_data_axis_gyr[X_REG],g_data_axis_gyr[Y_REG],g_data_axis_gyr[Z_REG]);
+}
+
+/*!
+ * @brief This API
+ */
+void bmi160_print_gyr_float(void)
+{
+	printf("gyr axis: X: %f Y: %f Z: %f\n", acc_device.x,acc_device.y,acc_device.z);
 }
--- main.c
+++ main.c
@@ -29,7 +29,7 @@
  */
  
 /**
- * @file    Practica1_Project.c
+ * @file    AHRS_Project.c
  * @brief   Application entry point.
  */
 #include <stdio.h>
@@ -39,9 +39,12 @@
 #include "clock_config.h"
 #include "MK64F12.h"
 #include "fsl_debug_console.h"
-#include "bmi160.h"
+#include "rtos_i2c.h"
+#include "rtos_uart.h"
 /* TODO: insert other include files here. */
-
+#include "FreeRTOS.h"
+#include "bmi160.h"
+#include "task.h"
 /* TODO: insert other definitions and declarations here. */
 
 /*
@@ -56,15 +59,47 @@ int main(void) {
   	/* Init FSL debug console. */
     BOARD_InitDebugConsole();
 
-    PRINTF("Hello World\n");
+    //PRINTF("Hello World\n");
 
-    gpio_i2c_config();
+    /*
+     * Config i2c & uart with RTOS
+     */
+    /**/
+    rtos_i2c_config_t i2c_config_t;
 
-    bmi160_normal_mode_config();
+    i2c_config_t.baudrate = RTOS_I2C_BAUDRATE;
+    i2c_config_t.i2c_number = rtos_i2c_0;
+    i2c_config_t.port = rtos_i2c_portE;
+    i2c_config_t.SDA_pin = bit_25;
+    i2c_config_t.SCL_pin = bit_24;
+    i2c_config_t.pin_mux = rtos_mux_alt05;
 
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-    	bmi160_get_data();
+    rtos_i2c_init(i2c_config_t);
+
+    /**/
+    rtos_uart_config_t uart_config_t;
+
+    uart_config_t.uart_number = rtos_uart0;
+    uart_config_t.baudrate = BD_115200;
+    uart_config_t.port = rtos_uart_portB;
+    uart_config_t.tx_pin = bit_17;
+    uart_config_t.rx_pin = bit_16;
+    uart_config_t.pin_mux = rtos_mux_alt3;
+
+    rtos_uart_init(uart_config_t);
+
+
+    /* BMI160_NORMAL_MODE task creation */
+    xTaskCreate(bmi160_normal_mode_config, "bmi160_normal_mode", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES-1, NULL);
+
+    /* BMI160_READ_ACC_GYR task creation */
+    xTaskCreate(bmi160_read_acc_gyr, "bmi160_read_acc", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES-2, NULL);
+    /* start scheduler */
+    vTaskStartScheduler();
+
+    /* Enter an infinite loop*/
+    for(;;)
+    {
     }
     return 0 ;
 }
